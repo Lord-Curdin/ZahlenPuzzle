0a3e0dc1b293f411c1d68fbdac414d7aa0791ddd
Lord-Curdin <lucuhu@gmail.com>
2017-01-24 14:41:24 +0100

    Add testproject

---
+++ ch.bbbaden.m426.Zahlenpuzzle.Test/GameTest.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ch.bbbaden.m426.Zahlenpuzzle.Test
+{
+    [TestClass]
+    public class GameTest
+    {
+        [TestMethod]
+        public void TestRestart()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
